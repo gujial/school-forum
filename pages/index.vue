ea3255158a4163b444d046bb20c72578caf0cb89
gujial <gu18647403665@outlook.com>
2025-08-03 22:04:53 +0800

    feat: 在首页展示背景图

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-card image="/card-image.jpg" class="d-flex align-center head-card" color="black">
+    <v-card :image="bgSrc" class="d-flex align-center head-card" color="black">
       <v-avatar v-if="src.length > 0" size="200">
         <v-img cover :src='src' />
       </v-avatar>
@@ -35,16 +35,27 @@ import TweetCard from '~/components/TweetCard.vue';
 const tweets = ref(null)
 const src = ref('')
 const username = ref('')
+const user_id = ref(null)
 const localePath = useLocalePath()
 const currentPage = useState('currentPage', () => 1)
 const pageCount = ref(1)
 const error = ref(null)
 const authError = ref(false)
+const bgSrc = ref('/card-image.jpg')
 
 const updateTweets = async () => {
   const data = await $fetch(`/api/tweets/order_by_time/${currentPage.value}`)
   tweets.value = data.data
   pageCount.value = data.maxPages
+}
+
+const updateBg = async () => {
+  try {
+    const data = await $fetch('/api/bg/' + user_id.value);
+    bgSrc.value = data.data || '/card-image.jpg';
+  } catch (err) {
+    bgSrc.value = '/card-image.jpg'
+  }
 }
 
 watch(currentPage, updateTweets)
@@ -53,7 +64,9 @@ onMounted(async () => {
   try {
     const data = await $fetch('/api/auth/user');
     username.value = data.user.username;
-  } catch(err) {
+    user_id.value = data.user.user_id;
+    updateBg();
+  } catch (err) {
     // 检查是否为 401 未认证错误
     if (err?.status === 401 || (err?.response && err.response.status === 401)) {
       authError.value = true
@@ -61,7 +74,7 @@ onMounted(async () => {
       error.value = err
     }
   }
-  updateTweets()
+  updateTweets();
 });
 </script>
 

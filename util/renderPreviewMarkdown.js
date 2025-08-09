c646a2d79be0b59004649704972a60c14d106130
gujial <gu18647403665@outlook.com>
2025-08-09 12:42:49 +0800

    feat: update hljs style for dark mode

--- util/renderPreviewMarkdown.js
+++ util/renderPreviewMarkdown.js
@@ -4,7 +4,7 @@ import { useColorMode } from '@vueuse/core';
 const colorMode = useColorMode();
 const renderMarkdown = (md, id) => {
   Vditor.preview(document.getElementById(id), md, {
-    hljs: { style: "github" },
+    hljs: { style: colorMode.value === 'dark' ? 'github-dark' : 'github' },
     theme: {
       current: colorMode.value === 'dark' ? 'dark' : 'classic',
     },

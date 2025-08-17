27d35ae16713e52ce8b81097e9625361f77774c2
gujial <gu18647403665@outlook.com>
2025-08-17 13:24:26 +0800

    feat: 添加获取其他用户的关注、粉丝列表的后端接口

---
+++ server/api/follow/get_follower_list/[id].ts
@@ -0,0 +1,49 @@
+import { defineEventHandler } from 'h3'
+import { useDatabase } from '../../../util/database'
+import authMiddleware from '../../../util/auth';
+
+export default defineEventHandler(async (event) => {
+    await authMiddleware(event)
+    const db = useDatabase()
+    const query = getQuery(event)
+    const page = query.page ? parseInt(query.page as string) : 1
+    const userId = getRouterParam(event, 'id');
+
+    if (page < 0) {
+        return {
+            success: false,
+            message: 'Wrong page number'
+        }
+    }
+
+    const limit = query.pageSize ? parseInt(query.pageSize as string) : 20
+    const offset = (page - 1) * limit
+
+    try {
+        const result = await db.sql`SELECT COUNT(follower_id) AS total FROM Follows WHERE following_id = ${userId}`
+        if (result.rows === undefined) {
+            throw new Error('Failed to retrieve tweet count')
+        }
+        const total = result.rows[0].total as number
+        const maxPages = Math.ceil(total / limit);
+
+        const { rows } = await db.sql`select follower_id from Follows where following_id = ${userId} LIMIT ${limit} OFFSET ${offset}`;
+
+        if (rows === undefined) {
+            throw new Error('Query returned undefined')
+        }
+
+        return {
+            success: true,
+            data: rows,
+            maxPages: maxPages,
+            total: total
+        }
+    } catch (error) {
+        console.error('Database error:', error)
+        return {
+            success: false,
+            message: 'Failed to follow user'
+        }
+    }
+})

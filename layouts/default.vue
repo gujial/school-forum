aef43f74acda81258ecdd5699721ff931a2d3d54
gujial <gu18647403665@outlook.com>
2025-08-21 22:42:48 +0800

    fix: 修复跟随系统切换主题功能异常

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,7 +9,6 @@
 
 <script setup lang="ts">
     import { onMounted, watch } from 'vue';
-    import { useColorMode } from '@vueuse/core';
     import { useTheme } from 'vuetify';
     import NavBar from '~/components/NavBar.vue';
 
@@ -20,11 +19,6 @@
     // 组件挂载时获取用户信息
     onMounted(async () => {
         await fetchAuthUser();
-        if (localStorage.getItem('nuxt-color-mode') === 'light') {
-            colorMode.value = 'light';
-        } else if (localStorage.getItem('nuxt-color-mode') === 'dark') {
-            colorMode.value = 'dark';
-        }
     });
 
     // 响应暗/亮主题变化

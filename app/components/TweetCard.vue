046ccd55a1f2445260f6a4418c441fbe6d95666c
gujial <gu18647403665@outlook.com>
2025-09-08 17:06:23 +0800

    fix: 修复卡片预览图片高度问题

--- app/components/TweetCard.vue
+++ app/components/TweetCard.vue
@@ -44,7 +44,7 @@
             hide-delimiters
         >
             <v-carousel-item v-for="image in images" :key="image.media_id">
-                <v-img :src="image.media_url" cover>
+                <v-img :src="image.media_url" cover height="100%">
                     <template #placeholder>
                         <div class="d-flex align-center justify-center fill-height">
                             <v-progress-circular
@@ -58,7 +58,7 @@
         </v-carousel>
         <v-carousel v-if="video != null" height="300px" cycle :show-arrows="false" hide-delimiters>
             <v-carousel-item>
-                <v-img :src="video" cover>
+                <v-img :src="video" cover height="100%">
                     <template #placeholder>
                         <div class="d-flex align-center justify-center fill-height">
                             <v-progress-circular
